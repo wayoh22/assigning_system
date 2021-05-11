73c125620dbaccae74e2a96190aaeb409c30be35
Ronnie <ronnie.tamburro@gmail.com>
2021-05-11 08:10:43 -0400

    scaffolded game model and made simple_form for game

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'devise'
 gem 'simple_form'
+gem "simple_calendar", "~> 2.4"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  resources :games
   get 'static/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "static#home"
+  root to: "games#index"
 end
