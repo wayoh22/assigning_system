8487c274cfa3ac38b1855833105aaaa8aeafe4f8
Ronnie <ronnie.tamburro@gmail.com>
2021-05-13 19:31:23 -0400

    Created welcome page controller/worked on welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   devise_for :referees, path: "referees", controller: { sessions: "referees/sessions" }
   devise_for :assignors, path: "assignors", controller: { sessions: "assignors/sessions" }
   resources :games
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "games#index"
+  root to: "welcome#index"
 end
