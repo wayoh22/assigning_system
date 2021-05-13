f01b04d8f91c3b80b96a36a27550984422d16912
Ronnie <ronnie.tamburro@gmail.com>
2021-05-13 15:02:32 -0400

    fixed datetime syntax

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Game.delete_all
+
 puts "Seeding..."
 
-Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Atletico ", away_team:"Inter Connecticut FC", start_time:Time.now.to_datetime, end_time:Time.now.to_datetime.end_of_day, location:"Stanwich School")
-Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Marseille", away_team:"Inter Connecticut FC", start_time:Time.now.to_datetime, end_time:Time.now.to_datetime.end_of_day, location:"Stanwich School")
-Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Manchester City", away_team:"Inter Connecticut FC", start_time:Time.now.to_datetime, end_time:Time.now.to_datetime.end_of_day, location:"Stanwich School")
-Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Chelsea", away_team:"Inter Connecticut FC", start_time:Time.now.to_datetime, end_time:Time.now.to_datetime.end_of_day, location:"Stanwich School")
-Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Barcelona", away_team:"Inter Connecticut FC", start_time:Time.now.to_datetime, end_time:Time.now.to_datetime.end_of_day, location:"Stanwich School")
-Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Verona", away_team:"Inter Connecticut FC", start_time:Time.now.to_datetime, end_time:Time.now.to_datetime.end_of_day, location:"Stanwich School")
+Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Atletico ", away_team:"Inter Connecticut FC", start_time:Time.now, end_time:Time.now + 7200, location:"Stanwich School")
+Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Marseille", away_team:"Inter Connecticut FC", start_time:Time.now, end_time:Time.now + 7200, location:"Stanwich School")
+Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Manchester City", away_team:"Inter Connecticut FC", start_time:Time.now, end_time:Time.now + 7200, location:"Stanwich School")
+Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Chelsea", away_team:"Inter Connecticut FC", start_time:Time.now, end_time:Time.now + 7200, location:"Stanwich School")
+Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Barcelona", away_team:"Inter Connecticut FC", start_time:Time.now, end_time:Time.now + 7200, location:"Stanwich School")
+Game.create(league:"EDP", home_team:"Chelsea Piers Shoreline SC Verona", away_team:"Inter Connecticut FC", start_time:Time.now, end_time:Time.now + 7200, location:"Stanwich School")
 
 puts "Seeding complete."
