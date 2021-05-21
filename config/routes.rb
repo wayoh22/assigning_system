a636369029bf67530997195e1d0af5795ec3b6f0
Ronnie <ronnie.tamburro@gmail.com>
2021-05-21 15:08:04 -0400

    afixed registrations controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 #
 
 Rails.application.routes.draw do 
-  devise_for :assignors, path: "assignors", controllers: {sessions: "assignors/sessions"}
-  devise_for :referees, path: "referees", controllers: {sessions: "referees/sessions"}
+  devise_for :assignors, path: "assignors", controllers: {registrations: "assignors/registrations", sessions: "assignors/sessions"}
+  devise_for :referees, path: "referees", controllers: {registrations: "referees/registrations", sessions: "referees/sessions"}
   resources :games
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
